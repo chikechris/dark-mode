e8e12986b87b2f7ef366df1198344d04c71843c0
Chike Chris <cchike2017@gmail.com>
2019-07-25 16:42:32 -0400

    built useLocalStorage function

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export const useLocalStorage = (key, initialValue) => {
+  if (typeof key !== "string")
+    throw new Error("Error: useLocalStorage(key, value)");
+
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    console.log(`useLocalStorage:useState`, key, initialValue, item);
+    return item ? JSON.parse(item) : initialValue;
+  });
+
+  const setValue = value => {
+    setStoredValue(value);
+    console.log(`useLocalStorage:useState:`, value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  return [storedValue, setValue];
+};
